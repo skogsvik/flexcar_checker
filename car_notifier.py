eec9872c8ee8707180920526527ac1adca70446d
Erik Silfverswärd <21970817+skogsvik@users.noreply.github.com>
2022-10-05 14:37:49 -0400

    Improve error handling

--- car_notifier.py
+++ car_notifier.py
@@ -96,8 +96,11 @@ def main(price=80, sleep=60):
 
 
 if __name__ == '__main__':
-    try:
-        main(80, 60)
-    except:
-        email_error()
-        raise
+    while True:
+        try:
+            main(80, 60)
+        except KeyboardInterrupt:
+            raise
+        except:
+            email_error()
+            time.sleep(300) # Wait 5 min before trying again to avoid too many emails
